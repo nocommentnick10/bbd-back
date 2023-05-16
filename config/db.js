1f746f291fdfb99f9f2dc01467ec462813c6cae2
Nick <kalinovne.io@gmail.com>
2023-05-16 13:15:51 +0600

    Finished project

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 const mysql = require('mysql2')
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'bbd',
-    password: ''
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD
 })
 
 module.exports = pool.promise()
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express')
 require('dotenv').config()
-const session = require('express-session')
 const mysql = require('mysql2')
-const MySQLSession = require('express-mysql-session')(session)
 
 const PORT = 8080
 
@@ -10,47 +8,13 @@ const app = express()
 
 app.use(express.json())
 
-// const options = {
-//     host: 'kalinovne.ru',
-//     port: 3306,
-//     user: 'u1884314_root',
-//     password: '12345678root',
-//     database: 'u1884314_bbd'
-// }
-
-// const sessionStore = new MySQLSession(options);
-
-// app.use(session({
-//     secret: 'key',
-//     resave: false,
-//     saveUninitialized: false,
-//     store: sessionStore
-// }))
-
-// const isAuth = (req, res, next) => {
-//     if(req.session.isAuth){
-//         next()
-//     } else {
-//         res.redirect('/')
-//     }
-// }
-
-// sessionStore.onReady().then(() => {
-// 	// MySQL session store ready for use.
-// 	console.log('MySQLStore ready');
-// }).catch(error => {
-// 	// Something went wrong.
-// 	console.error(error);
-// });
-
 app.use('/itemsinfo', require('./routes/itemsInfoRoute'))
 app.use('/items', require('./routes/itemsRoute'))
 app.use('/bids', require('./routes/bidsRoute'))
 app.use('/bidnew', require('./routes/bidNewRoute'))
-// app.use('/auth', require('./routes/signInRoute'))
 
 app.get('/', (req, res) => {
-    res.send('Hello')
+    res.send('<a href="itemsinfo">/itemsinfo</a> <br> <a href="items">/items</a> <br> <a href="items/5">/items/id</a> <br> <a href = "/items?complex=Речной&rooms=2,3&sqmin=40&sqmax=90&pricemin=2325000&pricemax=4525000">/items?filter</a> <br> <a href="bids">/bids</a> <br> POST /bidnew')
 })
 
 app.listen(PORT, () => { console.log(`Server is running at port ${PORT}`) })
